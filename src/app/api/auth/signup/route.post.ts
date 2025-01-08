622d8f1a289b8ecd29988e8f68872ea1e39346af
RenanYhuel <renanyhuel@gmail.com>
2025-01-08 20:15:12 +0100

    Add password hash to signup

--- src/app/api/auth/signup/route.post.ts
+++ src/app/api/auth/signup/route.post.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { z } from 'zod';
+import bcrypt from 'bcrypt'
 
 const schema = z.object({
     email: z.string().email(),
@@ -21,6 +22,14 @@ export default function handler(req: NextApiRequest,res: NextApiResponse
     }
 
     const { email, password, name, firstname } = result.data;
+
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, (err: Error | null, hash: string) => {
+        if (err) {
+            return res.status(500).json({ message: 'Erreur lors du hashage du mot de passe' });
+        }
+        console.log(hash);
+    });
 
     res.status(200).json({ message: 'Inscription réussie' });
 }
