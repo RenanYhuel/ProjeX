4d19ae3d5d99b6a882386dc541bffa7a28392a4d
RenanYhuel <renanyhuel@gmail.com>
2025-01-09 18:30:37 +0100

    Add the token JWT generation to send a verification mail

--- src/app/api/auth/signup/route.post.ts
+++ src/app/api/auth/signup/route.post.ts
@@ -1,7 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { z } from 'zod';
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
 const prisma = new PrismaClient();
 
 const schema = z.object({
@@ -11,16 +12,15 @@ const schema = z.object({
     firstname: z.string(),
 });
 
-export default async function handler(req: NextApiRequest,res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
-        return res.status(405).json({ message: 'Méthode HTTP non autorisée' });
+        return res.status(405).json({ message: 'HTTP method not allowed' });
     }
     const body = req.body;
     const result = schema.safeParse(body);
 
     if (!result.success) {
-        return res.status(400).json({ message: 'Données invalides' });
+        return res.status(400).json({ message: 'Invalid data' });
     }
 
     const { email, password, name, firstname } = result.data;
@@ -30,24 +30,34 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse
         },
     });
     if (userExists) {
-        return res.status(400).json({ message: 'Cet email est déjà associé à un compte' });
+        return res.status(400).json({ message: 'This email is already associated with an account' });
+    }
+
+    if (!process.env.JWT_SECRET) {
+        throw new Error('JWT_SECRET is not defined');
     }
 
     const saltRounds = 10;
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+    });
     try {
         const hash = await bcrypt.hash(password, saltRounds);
         await prisma.user.create({
             data: {
                 email,
                 password: hash,
-                name,
-                firstname,
+                lastName: name,
+                firstName: firstname,
+                isEmailVerified: false,
+                emailVerificationToken: token,
             },
         });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ message: 'Erreur lors du hashage du mot de passe ou de la création de l\'utilisateur' });
+        return res.status(500).json({ message: 'Error hashing password or creating user' });
     }
 
-    res.status(200).json({ message: 'Inscription réussie' });
+
+    res.status(200).json({ message: 'Registration successful' });
 }
