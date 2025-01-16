4a08444e85f6cfbe60fe5b5c2994aebb52d64f40
Renan Yhuel <renan.yhuel@gmail.com>
2025-01-16 20:51:02 +0100

    Adding the active sidebar system

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { SessionData } from '@/types/sessionData';
+import checkSession from '@/utils/checkSession';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
+
+export default function Home() {
+
+const router = useRouter();
+
+useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        checkSession(token).then((data: SessionData) => {
+            if (data.success == false) {
+            router.push('/auth/login');
+            }
+        });
+    } else {
+        router.push('/auth/login');
+    }
+}, [router]);
+
+return (
+    <div className=''>
+        Page Projects
+    </div>
+);
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,26 +2,28 @@
 
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Sidebar = () => {
+  const pathname = usePathname();
 
   return (
     <div className="min-h-full bg-[#f6f6f9] w-[230px] z-[2000] h-screen">
       <div className="flex h-16 w-full items-center justify-center p-[20px] text-[24px]">
         <span className="text-3xl font-extrabold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-            Proje<span className="text-purple-600">X</span>
+          Proje<span className="text-purple-600">X</span>
         </span>
       </div>
       <div>
         <ul className="mt-[48px] w-full">
-          <li className="h-[48px] bg-transparent ml-[6px] rounded-l-[48px] p-[4px] active sidebar">
-            <Link href="/" className="w-full h-full bg-[#f6f6f9] flex items-center rounded-[48px] text-[16px] text-[#363949] whitespace-nowrap ">
+          <li className={`h-[48px] bg-transparent ml-[6px] rounded-l-[48px] p-[4px] sidebar ${pathname === '/' ? 'active' : ''}`}>
+            <Link href="/" className="w-full h-full bg-[#f6f6f9] flex items-center rounded-[48px] text-[16px] text-[#363949] whitespace-nowrap">
               <span className="material-symbols-outlined ml-3">dashboard</span>
               <span className="ml-2">Dashboard</span>
             </Link>
           </li>
-          <li className="h-[48px] bg-transparent ml-[6px] rounded-l-[48px] p-[4px] sidebar">
-            <Link href="" className="w-full h-full bg-[#f6f6f9] flex items-center rounded-[48px] text-[16px] text-[#363949] whitespace-nowrap ">
+          <li className={`h-[48px] bg-transparent ml-[6px] rounded-l-[48px] p-[4px] sidebar ${pathname.includes('/projects') ? 'active' : ''}`}>
+            <Link href="/projects" className="w-full h-full bg-[#f6f6f9] flex items-center rounded-[48px] text-[16px] text-[#363949] whitespace-nowrap">
               <span className="material-symbols-outlined ml-3">folder</span>
               <span className="ml-2">Projects</span>
             </Link>
